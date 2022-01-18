30fdc6f51ad3159fff3b039a6add37b41d0493e7
Brian Garrel <75063749+briangarrel@users.noreply.github.com>
2022-01-18 18:44:24 -0300

    update main

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ let weather = {
         document.querySelector(".humidity").innerText = `Humedad: ${temp}%`;
         document.querySelector(".wind").innerText = `Viento: ${speed} km/h`;
         document.querySelector(".weather").classList.remove("loading")
+        document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${name})`
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
